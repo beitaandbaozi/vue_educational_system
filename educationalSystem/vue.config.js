8a88a02c2466aae1f3a6f3697ad93b14cf6e0409
beitaandbaozi <2280496040@qq.com>
2021-10-19 23:35:26 +0800

    完全配置跨域，实现登录功能

--- educationalSystem/vue.config.js
+++ educationalSystem/vue.config.js
@@ -41,7 +41,11 @@ module.exports = {
             // 当我们的本地的请求 有/api的时候，就会代理我们的请求地址向另外一个服务器发出请求
             '/api': {
                 target: 'http://127.0.0.1:3000/', // 跨域请求的地址
-                changeOrigin: true // 只有这个值为true的情况下 才表示开启跨域
+                changeOrigin: true, // 只有这个值为true的情况下 才表示开启跨域
+                pathRewrite: {
+                    // 重新路由  localhost:8888/api/login  => www.baidu.com/api/login
+                    '^/api': '' // 假设我们想把 localhost:8888/api/login 变成www.baidu.com/login 就需要这么做 
+                } 
             }
         }
 
