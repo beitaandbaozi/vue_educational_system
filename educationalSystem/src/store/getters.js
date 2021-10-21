a2bdff56aef96fe6b6d87e59f2c37cb448704e75
beitaandbaozi <2280496040@qq.com>
2021-10-21 16:45:54 +0800

    用户状态共享

--- educationalSystem/src/store/getters.js
+++ educationalSystem/src/store/getters.js
@@ -1,7 +1,8 @@
 const getters = {
-  sidebar: state => state.app.sidebar,
-  device: state => state.app.device,
-  token: state => state.user.token,
-  name:state => state.user.userInfo.username //建立用户名称的映射
+    sidebar: state => state.app.sidebar,
+    device: state => state.app.device,
+    token: state => state.user.token,
+    name: state => state.user.userInfo.username, //建立用户名称的映射
+    userId: state => state.user.userInfo.id
 }
 export default getters
