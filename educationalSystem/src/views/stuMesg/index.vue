b3db43b457166aaf2bc314507d9edc43e6cbdfe3
beitaandbaozi <2280496040@qq.com>
2021-10-23 15:39:53 +0800

    创建登录为角色时所需要的页面和对应的路由文件

---
+++ educationalSystem/src/views/stuMesg/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="dashboard-container">
+    <div class="app-container">
+      <h2>
+        个人信息
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
