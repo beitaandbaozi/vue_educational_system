f39a5b1fb54dbfa188a95f665eed06362c18bb02
beitaandbaozi <2280496040@qq.com>
2021-10-27 13:56:44 +0800

    封装学生api接口

--- edcuationalServe/serve/app.js
+++ edcuationalServe/serve/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login')
 var usersRouter = require('./routes/users');
+var studentRouter = require('./routes/student')
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/login',loginRouter)
 app.use('/users', usersRouter);
+app.use('/student',studentRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- educationalSystem/src/views/stuMesg/index.vue
+++ educationalSystem/src/views/stuMesg/index.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        个人信息
-      </h2>
+        <el-descriptions title="用户信息">
+          <el-descriptions-item label="用户名">kooriookami</el-descriptions-item>
+          <el-descriptions-item label="手机号">18100000000</el-descriptions-item>
+          <el-descriptions-item label="居住地">苏州市</el-descriptions-item>
+          <el-descriptions-item label="备注">
+            <el-tag size="small">学校</el-tag>
+          </el-descriptions-item>
+          <el-descriptions-item label="联系地址">江苏省苏州市吴中区吴中大道 1188 号</el-descriptions-item>
+        </el-descriptions>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
