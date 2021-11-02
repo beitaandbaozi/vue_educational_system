51057bd78e72ffae439cdeb7eaba5d41982e43d7
beitaandbaozi <2280496040@qq.com>
2021-11-02 19:31:31 +0800

    封装课程表中将数字转中文的方法

--- educationalSystem/src/utils/validate.js
+++ educationalSystem/src/utils/validate.js
@@ -7,7 +7,7 @@
  * @returns {Boolean}
  */
 export function isExternal(path) {
-  return /^(https?:|mailto:|tel:)/.test(path)
+    return /^(https?:|mailto:|tel:)/.test(path)
 }
 
 /**
@@ -15,6 +15,33 @@ export function isExternal(path) {
  * @returns {Boolean}
  */
 export function validUsername(str) {
-  const valid_map = ['admin', 'editor']
-  return valid_map.indexOf(str.trim()) >= 0
+    const valid_map = ['admin', 'editor']
+    return valid_map.indexOf(str.trim()) >= 0
+}
+
+/**
+     * 数字转中文
+     * @param {Number} num 需要转换的数字
+     * @param {String} identifier 标识符
+     * @returns {String} 转换后的中文
+     */
+export function  digital2Chinese(num, identifier) {
+    const character = [
+        "零",
+        "一",
+        "二",
+        "三",
+        "四",
+        "五",
+        "六",
+        "七",
+        "八",
+        "九",
+        "十",
+        "十一",
+        "十二",
+    ];
+    return identifier === "week" && (num === 0 || num === 7)
+        ? "日"
+        : character[num];
 }
