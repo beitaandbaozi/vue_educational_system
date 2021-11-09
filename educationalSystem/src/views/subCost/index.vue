5d9aca42d4f520f49742d878a8b2c87891433a75
beitaandbaozi <2280496040@qq.com>
2021-11-09 21:12:07 +0800

    交费历史页面封装完成

--- educationalSystem/src/views/subCost/index.vue
+++ educationalSystem/src/views/subCost/index.vue
@@ -1,19 +1,73 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        预缴费用
-      </h2>
+      <el-table
+        :data="subCostData"
+        border
+        style="width: 100%"
+      >
+        <el-table-column
+          label="交费日期"
+          width="180"
+          prop="sub_date"
+          align="center"
+        >
+        </el-table-column>
+        <el-table-column
+          label="票据号"
+          width="180"
+          prop="bill_num"
+          align="center"
+        >
+        </el-table-column>
+        <el-table-column
+          label="学年"
+          prop="academic"
+          align="center"
+        >
+        </el-table-column>
+        <el-table-column
+          label="费用名称"
+          prop="name"
+          align="center"
+        >
+        </el-table-column>
+        <el-table-column
+          label="交费金额"
+          prop="sub_money"
+          align="center"
+        >
+        </el-table-column>
+        <el-table-column
+          label="交费性质"
+          prop="sub_type"
+          align="center"
+        >
+        </el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
 
 <script>
+import { getSubCost } from "@/api/subCost";
 export default {
-
-}
+  data() {
+    return {
+      subCostData: [],
+    };
+  },
+  created() {
+    this.getSubCost();
+  },
+  methods: {
+    async getSubCost() {
+      let res = await getSubCost();
+      this.subCostData = res.result;
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
