de9b21f78716bd37f4f7eccb1303de9c1bee86b1
beitaandbaozi <2280496040@qq.com>
2021-11-09 11:59:25 +0800

    考试时间页面封装完成

--- educationalSystem/src/views/testTime/index.vue
+++ educationalSystem/src/views/testTime/index.vue
@@ -1,45 +1,95 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <el-collapse
-      >
-        <el-collapse-item
-          title="一致性 Consistency"
-          name="1"
+      <!-- 个人信息 -->
+      <el-card>
+        <el-row
+          type="flex"
+          justify="space-around"
         >
-          <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
-          <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
-        </el-collapse-item>
-        <el-collapse-item
-          title="反馈 Feedback"
-          name="2"
-        >
-          <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
-          <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
-        </el-collapse-item>
-        <el-collapse-item
-          title="效率 Efficiency"
-          name="3"
-        >
-          <div>简化流程：设计简洁直观的操作流程；</div>
-          <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
-          <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
-        </el-collapse-item>
-        <el-collapse-item
-          title="可控 Controllability"
-          name="4"
-        >
-          <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
-          <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
-        </el-collapse-item>
-      </el-collapse>
+          <el-col :span="6">
+            学号：{{stuForm.number}}
+          </el-col>
+          <el-col :span="6">
+            姓名：{{stuForm.name}}
+          </el-col>
+          <el-col :span="6">
+            专业：{{stuForm.major}}
+          </el-col>
+        </el-row>
+      </el-card>
+      <!-- 考试时间信息 -->
+      <el-card style="margin-top:20px">
+        <el-collapse style="padding:20px">
+          <el-collapse-item
+            v-for="(item) in testList"
+            :key="item.cno"
+            :title="item.name"
+          >
+            <el-row
+              type="flex"
+              justify="space-between"
+            >
+              <el-col>
+                课程代码：{{item.cno}}
+              </el-col>
+              <el-col>
+                课程名称：{{item.name}}
+              </el-col>
+              <el-col>
+                课程所属系别：{{item.duty}}
+              </el-col>
+              <el-col>
+                课程学分：{{item.credit}}
+              </el-col>
+              <el-col style="color:red;font-weight:bold;">
+                考试时间：{{item.test_time}}
+              </el-col>
+            </el-row>
+          </el-collapse-item>
+        </el-collapse>
+      </el-card>
+
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { getStuInfo } from "@/api/stuMesg";
+import { getTestTime } from "@/api/testTime";
+export default {
+  data() {
+    return {
+      stuForm: {
+        number: "",
+        name: "",
+        major: "",
+      },
+      // 考试时间列表
+      testList: [],
+    };
+  },
+  created() {
+    this.getStuInfo();
+    this.getTestTime();
+  },
+  methods: {
+    //  获取个人信息
+    async getStuInfo() {
+      let res = await getStuInfo();
+      // 给表单赋值
+      this.stuForm = res;
+    },
+    async getTestTime() {
+      let res = await getTestTime();
+      this.testList = res.result;
+    },
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.el-collapse {
+  border: 0px;
+}
 </style>
