29eb1a05804e76efa2ce42364856f0d4b6e2302d
beitaandbaozi <2280496040@qq.com>
2021-11-22 16:45:46 +0800

    封装通用工具栏（插槽）为公共组件并注册

--- educationalSystem/src/components/index.js
+++ educationalSystem/src/components/index.js
@@ -2,9 +2,10 @@
  * 组件注册
  */
 import TagsView from './TagsView'
+import PageTools from './PageTools'
 export default {
     install(Vue) {
         Vue.component('TagsView',TagsView)
-
+        Vue.component('PageTools',PageTools)
     }
 }
