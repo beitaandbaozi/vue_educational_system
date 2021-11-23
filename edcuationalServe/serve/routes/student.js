06f042dab04159c32aef33eb4222dd8998027c0a
beitaandbaozi <2280496040@qq.com>
2021-11-23 14:55:18 +0800

    将用户角色为学生的交费历史的后端api迁移到student路由中

--- edcuationalServe/serve/routes/student.js
+++ edcuationalServe/serve/routes/student.js
@@ -77,6 +77,29 @@ router.post('/getOptionalCourseInfo', function (req, res) {
         }
     })
 })
+// 获取学生交费记录
+router.post('/getSubCost', function (req, res) {
+    let { authorization } = req.headers
+    const tokenData = jwt.decode(authorization.split(' ')[1].slice(0, -1));
+    const { name } = tokenData;
+    let sql = 'select * from submit_cost where num = ? order by academic desc';
+    db.query(sql, [name], function (err, result) {
+        if (err) {
+            console.log('获取交费记录时数据库查询出错！');
+            return
+        } else {
+            // console.log(result)
+            res.send({
+                status: 200,
+                msg: '获取交费记录成功！',
+                data: {
+                    result
+                }
+            })
+        }
+    })
+
+})
 
 // 获取学生的课程通知（首页中应用）
 router.post('/getTeachingTaskByStu', function (req, res) {
--- educationalSystem/src/api/subCost.js
+++ educationalSystem/src/api/subCost.js
@@ -1,7 +1,7 @@
 import request from '@/utils/request'
 export function getSubCost(){
     return request({
-        url:'/cost/getSubCost',
+        url:'/student/getSubCost',
         method: 'post'
     })
 }
