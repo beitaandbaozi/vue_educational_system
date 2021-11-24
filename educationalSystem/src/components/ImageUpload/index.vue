d0c07a4e5a6f786e6df3fc61fdfa9094c6cb4a42
beitaandbaozi <2280496040@qq.com>
2021-11-24 09:23:59 +0800

    创建图片上传组件并进行注册

---
+++ educationalSystem/src/components/ImageUpload/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-upload list-type="picture-card">
+    <i class="el-icon-plus" />
+  </el-upload>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- educationalSystem/src/components/index.js
+++ educationalSystem/src/components/index.js
@@ -3,9 +3,11 @@
  */
 import TagsView from './TagsView'
 import PageTools from './PageTools'
+import ImageUpload from './ImageUpload'
 export default {
     install(Vue) {
         Vue.component('TagsView',TagsView)
         Vue.component('PageTools',PageTools)
+        Vue.component('ImageUpload',ImageUpload)
     }
 }
