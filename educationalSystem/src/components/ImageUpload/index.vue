1e7cea979e9b17244ecc479f4790b85151bcce70
beitaandbaozi <2280496040@qq.com>
2021-11-24 09:41:20 +0800

    完成图片上传组件中图片预览的功能

--- educationalSystem/src/components/ImageUpload/index.vue
+++ educationalSystem/src/components/ImageUpload/index.vue
@@ -1,18 +1,54 @@
 <template>
-  <el-upload list-type="picture-card">
-    <i class="el-icon-plus" />
-  </el-upload>
+  <div>
+    <el-upload
+      list-type="picture-card"
+      :limit="1"
+      action="#"
+      :on-preview="preview"
+    >
+      <i class="el-icon-plus" />
+    </el-upload>
+    <!-- 预览弹层 -->
+    <el-dialog
+      title="图片"
+      :visible.sync="showDialog"
+    >
+      <img
+        :src="imgUrl"
+        style="width:100%"
+        alt=""
+      >
+    </el-dialog>
+  </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    // 设定一个计算属性 判断是否已经上传完了一张
+    fileComputed() {
+      return this.fileList.length === 1;
+    },
+  },
   data() {
-    return {};
+    return {
+      // 图片地址
+      fileList: [],
+      // 控制显示弹层
+      showDialog: false,
+      imgUrl: "",
+    };
   },
 
   mounted() {},
 
-  methods: {},
+  methods: {
+    //  预览图片
+    preview(file) {
+      this.imgUrl = file.url;
+      this.showDialog = true;
+    },
+  },
 };
 </script>
 
