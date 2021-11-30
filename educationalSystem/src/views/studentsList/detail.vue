4f210e6631ff0080e03eb2eded6a7e2b7f6916f1
beitaandbaozi <2280496040@qq.com>
2021-11-30 16:50:04 +0800

    完成学生详细页面的基础布局

--- educationalSystem/src/views/studentsList/detail.vue
+++ educationalSystem/src/views/studentsList/detail.vue
@@ -1,27 +1,148 @@
 <template>
-    <div>
-        
+  <div class="dashboard-container">
+    <div class="app-container">
+      <!-- 添加一个编辑资料 再统一修改 -->
+      <el-card>
+        <el-form
+          :model="studentInfo"
+          label-width="80px"
+        >
+          <!-- 头像信息 -->
+          <el-row
+            type="flex"
+            justify="end"
+          >
+            <img
+              src="@/assets/common/header.png"
+              alt="header"
+              class="header"
+            >
+          </el-row>
+          <!-- 基础信息 -->
+          <el-divider
+            content-position="left"
+            class="divider"
+          >基础信息</el-divider>
+          <el-row
+            type="flex"
+            justify="space-between"
+          >
+            <el-form-item label="学号">
+              <el-input
+                v-model="studentInfo.id"
+                disabled
+                size="mini"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="姓名">
+              <el-input
+                v-model="studentInfo.stu_name"
+                size="mini"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="系别">
+              <el-input
+                v-model="studentInfo.duty"
+                size="mini"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="年级">
+              <el-input
+                v-model="studentInfo.grad"
+                size="mini"
+              ></el-input>
+            </el-form-item>
+          </el-row>
+          <!-- 班级信息 -->
+          <el-divider
+            content-position="left"
+            class="divider"
+          >班级信息</el-divider>
+          <el-row
+            type="flex"
+            justify="space-between"
+          >
+            <el-form-item label="专业">
+              <el-input
+                v-model="studentInfo.major"
+                size="mini"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="行政班级">
+              <el-input
+                v-model="studentInfo.class"
+                size="mini"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="导生">
+              <el-input
+                v-model="studentInfo.advisor"
+                size="mini"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="辅导员">
+              <el-input
+                v-model="studentInfo.counsellor"
+                size="mini"
+              ></el-input>
+            </el-form-item>
+          </el-row>
+          <!-- 通讯信息 -->
+          <el-divider
+            content-position="left"
+            class="divider"
+          >通讯信息</el-divider>
+          <el-row
+            type="flex"
+            justify="space-between"
+          >
+            <el-form-item label="身份证">
+              <el-input
+                v-model="studentInfo.idCard"
+                size="mini"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="邮件">
+              <el-input
+                v-model="studentInfo.email"
+                size="mini"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="宿舍号">
+              <el-input
+                v-model="studentInfo.dormitory"
+                size="mini"
+              ></el-input>
+            </el-form-item>
+          </el-row>
+        </el-form>
+      </el-card>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            
-        };
-    },
-
-    mounted() {
-        
-    },
-
-    methods: {
-        
-    },
+  data() {
+    return {
+      //路由信息中取下来的id值
+      num: this.$route.params.id,
+      studentInfo: {},
+    };
+  },
+  methods: {},
 };
 </script>
 
 <style lang="scss" scoped>
-
+.header {
+  border-radius: 50%;
+}
+.divider {
+  margin: 30px 0px;
+  .el-divider__text {
+    font-size: 18px;
+    color: lightcoral;
+  }
+}
 </style>
