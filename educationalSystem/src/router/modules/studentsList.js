5c651c28d148182c59bb9b05cb3f4da0d171f58e
beitaandbaozi <2280496040@qq.com>
2021-11-30 15:56:42 +0800

    为学生详情创建路由和页面

--- educationalSystem/src/router/modules/studentsList.js
+++ educationalSystem/src/router/modules/studentsList.js
@@ -2,18 +2,27 @@ import Layout from '@/layout'
 //  {  path: '', component: '' }
 // 每个子模块 其实 都是外层是layout  组件位于layout的二级路由里面
 export default {
-  path: '/studentsList', // 路径
-  name: 'studentsList', // 给路由规则加一个name
-  component: Layout, // 组件
-  // 配置二级路的路由表
-  children: [{
-    path: '', // 这里当二级路由的path什么都不写的时候 表示该路由为当前二级路由的默认路由
-    component: () => import('@/views/studentsList'),
-    // 路由元信息  其实就是存储数据的对象 我们可以在这里放置一些信息
-    meta: {
-      title: '学生管理', // meta属性的里面的属性 随意定义 但是这里为什么要用title呢， 因为左侧导航会读取我们的路由里的meta里面的title作为显示菜单名称
-      icon:'studentsList',
-      roles: ['admin']
-    }
-  }]
+    path: '/studentsList', // 路径
+    name: 'studentsList', // 给路由规则加一个name
+    component: Layout, // 组件
+    // 配置二级路的路由表
+    children: [{
+        path: '', // 这里当二级路由的path什么都不写的时候 表示该路由为当前二级路由的默认路由
+        component: () => import('@/views/studentsList'),
+        // 路由元信息  其实就是存储数据的对象 我们可以在这里放置一些信息
+        meta: {
+            title: '学生管理', // meta属性的里面的属性 随意定义 但是这里为什么要用title呢， 因为左侧导航会读取我们的路由里的meta里面的title作为显示菜单名称
+            icon: 'studentsList',
+            roles: ['admin']
+        }
+    },
+    {
+        path: 'detail/:id', // query传参 动态路由传参
+        component: () => import('@/views/studentsList/detail'),
+        hidden: true, // 不在左侧菜单显示
+        meta: {
+            title: '学生详情' // 标记当前路由规则的中文名称 后续在做左侧菜单时 使用
+        }
+    },
+    ]
 }
