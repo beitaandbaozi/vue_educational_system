1b3a22595e7939099caa32e913465ec8ba5a4dcb
beitaandbaozi <2280496040@qq.com>
2021-12-01 10:48:57 +0800

    全局注册打印插件

--- educationalSystem/src/components/index.js
+++ educationalSystem/src/components/index.js
@@ -4,10 +4,12 @@
 import TagsView from './TagsView'
 import PageTools from './PageTools'
 import ImageUpload from './ImageUpload'
+import Print from 'vue-print-nb'
 export default {
     install(Vue) {
         Vue.component('TagsView',TagsView)
         Vue.component('PageTools',PageTools)
         Vue.component('ImageUpload',ImageUpload)
+        Vue.use(Print)
     }
 }
