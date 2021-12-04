ce9b58f021be4e7ffe5c4d5dd0c2dc0186881b6c
beitaandbaozi <2280496040@qq.com>
2021-12-04 16:23:27 +0800

    “广软快讯”后端API完成

--- edcuationalServe/serve/routes/new.js
+++ edcuationalServe/serve/routes/new.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const db = require('../db/index');
 
+// 获取全部的新闻信息
 router.post('/getAllNew', function (req, res) {
     let pagenum = Number(req.body.pagenum);
     let start, pagesize = Number(req.body.pagesize);
@@ -28,5 +29,25 @@ router.post('/getAllNew', function (req, res) {
         }
     })
 })
+// 根据id获取对应的新闻信息
+router.get('/getNewById/:id', function (req, res) {
+    let id = req.params.id;
+    let sql = 'select * from new where id = ?';
+    db.query(sql, [id], function (err, result) {
+        if (err) {
+            console.log('根据id获取对应的新闻信息时数据库出错');
+            return
+        } else {
+            res.send({
+                status: 200,
+                msg: '根据id获取对应的新闻信息成功！',
+                data: {
+                    result,
+                }
+            })
+        }
 
+    })
+
+})
 module.exports = router;
