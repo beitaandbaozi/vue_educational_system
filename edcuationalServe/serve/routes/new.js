8964a8585dcce02ff4285f3917f209dbf7b1e648
beitaandbaozi <2280496040@qq.com>
2021-12-04 15:35:56 +0800

    “广软快讯”后端API

--- edcuationalServe/serve/routes/new.js
+++ edcuationalServe/serve/routes/new.js
@@ -1,4 +1,32 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../db/index');
+
+router.post('/getAllNew', function (req, res) {
+    let pagenum = Number(req.body.pagenum);
+    let start, pagesize = Number(req.body.pagesize);
+    if (pagenum == undefined) {
+        pagenum = 1;
+        start = 0;
+    } else {
+        start = (pagenum - 1) * pagesize;
+    }
+    let sql = `select *,(select count(*) from new) as count from new limit ?,?`;
+    db.query(sql, [start, pagesize], function (err, result) {
+        if (err) {
+            console.log('获取所有新闻时数据库出错！')
+            return
+        } else {
+            res.send({
+                status: 200,
+                msg: '获取所有新闻成功！',
+                data: {
+                    result,
+                    count: result[0].count
+                }
+            })
+        }
+    })
+})
+
 module.exports = router;
