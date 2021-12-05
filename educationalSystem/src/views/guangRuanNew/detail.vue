43a8f98238b4f9510b9e76ee7ef4420b3a569eda
beitaandbaozi <2280496040@qq.com>
2021-12-05 15:38:56 +0800

    “广软快讯”详情页基本布局完成

--- educationalSystem/src/views/guangRuanNew/detail.vue
+++ educationalSystem/src/views/guangRuanNew/detail.vue
@@ -1,27 +1,83 @@
 <template>
-    <div>
-        {{$route.params.id}}
+  <div class="dashboard-container">
+    <div class="app-container">
+      <el-container>
+        <!-- 标题 -->
+        <el-header class="el-header">{{newList[0].title}}</el-header>
+        <!-- 稿件信息 -->
+        <el-main class="el-main-card">
+          <el-card shadow="never">
+            <el-row
+              type="flex"
+              justify="space-between"
+            >
+              <el-col>供稿部门：{{newList[0].depart_feed}}</el-col>
+              <el-col>供稿人：{{newList[0].contributor | capitalize }}</el-col>
+              <el-col>摄影：{{newList[0].photographer | capitalize }}</el-col>
+              <el-col>编辑：{{newList[0].edit}}</el-col>
+              <el-col>复审：{{newList[0].recheck}}</el-col>
+              <el-col>审核：{{newList[0].audit}}</el-col>
+            </el-row>
+            <br>
+            <el-row>
+              发布时间：{{newList[0].time}}
+            </el-row>
+          </el-card>
+        </el-main>
+        <!-- 主要内容 -->
+        <el-main class="el-main-content">{{newList[0].content}}</el-main>
+        <!-- 尾部 -->
+        <el-footer class="el-footer">（供稿：{{newList[0].depart_feed}} 初审：{{newList[0].edit}} 复审：{{newList[0].recheck}} 审核：{{newList[0].audit}}）</el-footer>
+      </el-container>
     </div>
+  </div>
 </template>
 
 <script>
+import { getNewById } from "@/api/guangRuanNew";
 export default {
-    data() {
-        return {
-            
-        };
+  filters: {
+    capitalize(val) {
+      if (!val) {
+        return "";
+      }
     },
-
-    mounted() {
-        
+  },
+  data() {
+    return {
+      id: this.$route.params.id,
+      newList: [],
+    };
+  },
+  created() {
+    this.getNewById();
+  },
+  methods: {
+    async getNewById() {
+      let res = await getNewById(this.id);
+      this.newList = res.result;
     },
-
-    methods: {
-        
-    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-
+.el-header {
+  text-align: center;
+  font-size: 22px;
+  font-weight: bold;
+}
+.el-main-card {
+  margin-top: -20px;
+}
+.el-main-content {
+  text-indent: 2em;
+  font-size: 18px;
+  color: #333;
+  line-height: 2em;
+}
+.el-footer {
+  text-align: center;
+  margin-top:20px
+}
 </style>
